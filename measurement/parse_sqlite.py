30cb818961dd341e2c1e139f178aac5ec1e97547
Ferenc Tükör <ferenc.tukor@radwaytech.services>
2025-06-20 23:45:26 +0200

    Fix calculating kernel runtime percentages

--- measurement/parse_sqlite.py
+++ measurement/parse_sqlite.py
@@ -1,6 +1,7 @@
 import argparse
 import sqlite3
 
+
 def get_cuda_events_for_kernel(db_path, kernel_name):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
@@ -17,13 +18,37 @@ def get_cuda_events_for_kernel(db_path, kernel_name):
 
     return [{"name": row[0], "duration_ms": row[1]} for row in rows]
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--db_file", type=str, required=True, help="Path to the SQLite database file.")
-    parser.add_argument("--target_kernels", type=str, nargs='+', required=True, help="List of kernel names to analyze.")
-    parser.add_argument("--end_to_end_runtime_ms", type=float, required=True, help="Total runtime in milliseconds.")
-    parser.add_argument("--target_runtime_ms", type=float, required=True, help="Total runtime of measurement target in milliseconds.")
-    parser.add_argument("--measurement_iteration_count", type=int, required=True, help="Number of measurement iterations.")
+    parser.add_argument(
+        "--db_file", type=str, required=True, help="Path to the SQLite database file."
+    )
+    parser.add_argument(
+        "--target_kernels",
+        type=str,
+        nargs="+",
+        required=True,
+        help="List of kernel names to analyze.",
+    )
+    parser.add_argument(
+        "--end_to_end_runtime_ms",
+        type=float,
+        required=True,
+        help="Total runtime in milliseconds.",
+    )
+    parser.add_argument(
+        "--target_runtime_ms",
+        type=float,
+        required=True,
+        help="Total runtime of measurement target in milliseconds.",
+    )
+    parser.add_argument(
+        "--measurement_iteration_count",
+        type=int,
+        required=True,
+        help="Number of measurement iterations.",
+    )
     args = parser.parse_args()
 
     kernel_duration_averages = {}
@@ -42,11 +67,19 @@ if __name__ == "__main__":
             kernel_duration_averages[target_kernel] += event["duration_ms"]
 
         if target_kernel in kernel_duration_averages:
-            kernel_duration_averages[target_kernel] /= actual_iteration_count
-            kernel_duration_percentages[target_kernel] = kernel_duration_averages[target_kernel] / args.target_runtime_ms * 100
+            kernel_duration_percentages[target_kernel] = (
+                kernel_duration_averages[target_kernel]
+                / args.measurement_iteration_count
+                / args.target_runtime_ms
+                * 100
+            )
 
-    cumulative_kernel_duration_percentages = f"{sum([x for x in kernel_duration_percentages.values()])} %"
-    kernel_duration_percentages = { k: f"{v} %" for k, v in kernel_duration_percentages.items() }
+    cumulative_kernel_duration_percentages = (
+        f"{sum([x for x in kernel_duration_percentages.values()])} %"
+    )
+    kernel_duration_percentages = {
+        k: f"{v} %" for k, v in kernel_duration_percentages.items()
+    }
 
     print(args.db_file)
     print(f"{args.end_to_end_runtime_ms} ms")
@@ -54,4 +87,3 @@ if __name__ == "__main__":
     print(f"{args.target_runtime_ms / args.end_to_end_runtime_ms * 100} %")
     print(kernel_duration_percentages)
     print(cumulative_kernel_duration_percentages)
-
