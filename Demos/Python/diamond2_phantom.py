e9d75414201fc138c5b043b3a52d7e26a19fcadc
Ferenc Tükör <ferenc.tukor@radwaytech.services>
2025-12-09 16:17:03 +0100

    Fix phantom generation and pipeline Recon now actually runs on it

--- Demos/Python/diamond2_phantom.py
+++ Demos/Python/diamond2_phantom.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# httomo run diamond2_phantom.nxs ~/work/httomo_pipeline/diamond2_phantom.yaml ~/work/output
+# httomo run diamond2_phantom.nxs ~/work/ToMoBAR/Demos/Python/diamond2_phantom.yaml ~/work/output
 import timeit
 import os
 import matplotlib.pyplot as plt
@@ -17,8 +17,10 @@ from httomo.methods import save_intermediate_data
 from httomo.runner.auxiliary_data import AuxiliaryData
 from httomo.runner.dataset import DataSetBlock
 
-plot = False
-# plot = True
+# plot = False
+plot = True
+# recon = False
+recon = True
 
 print("Building 3D phantom using TomoPhantom software")
 tic = timeit.default_timer()
@@ -26,6 +28,7 @@ model = 13  # select a model number from the library
 N_size = 256  # Define phantom dimensions using a scalar value (cubic phantom)
 # N_size = 2048  # Define phantom dimensions using a scalar value (cubic phantom) -- Limit for machines with 32 GB RAM
 # N_size = 4096  # Define phantom dimensions using a scalar value (cubic phantom) -- Diamond2 resolution
+# The biggest camera so far (that we are still commissioning) is 4416 x 2368, and if we use it in 2FOV mode that will be a uint16 4416 x 2368 x 3600
 path = os.path.dirname(tomophantom.__file__)
 path_library3D = os.path.join(path, "phantomlib", "Phantom3DLibrary.dat")
 
@@ -62,16 +65,16 @@ angles = np.linspace(0.0, 179.9, angles_num, dtype="float32")  # in degrees
 angles_rad = angles * (np.pi / 180.0)
 
 print("Generate 3D analytical projection data with TomoPhantom")
-projData3D_analyt = TomoP3D.ModelSino(
-    model, N_size, Horiz_det, Vert_det, angles, path_library3D
-)
+# projData3D_analyt = TomoP3D.ModelSino(model, N_size, Horiz_det, Vert_det, angles, path_library3D)
+projData3D_analyt = TomoP3D.ModelSinoSub(model, N_size, Horiz_det, Vert_det, (int(0.5 *Vert_det) + 0, int(0.5 *Vert_det) + 10), angles, path_library3D)
 
 print(f"#slices: {Vert_det}, #projections: {angles_num}, detector width: {Horiz_det}")
 print(f"sinogram shape: {projData3D_analyt.shape}")
 if plot:
     plt.figure()
     plt.subplot(131)
-    plt.imshow(projData3D_analyt[int(0.5 * Vert_det), :, :])
+    # plt.imshow(projData3D_analyt[int(0.5 * Vert_det), :, :])
+    plt.imshow(projData3D_analyt[int(0.5 * 10), :, :])
     plt.title("sinogram, axial view")
 
     plt.subplot(132)
@@ -83,6 +86,8 @@ if plot:
     plt.title("sinogram, sagittal view")
     plt.show()
 
+swapped_projData3D_analyt = np.swapaxes(projData3D_analyt, 0, 1)
+print(f"swapped sinogram shape: {projData3D_analyt.shape}")
 
 def save_nxs(filepath: Path, angles, data, detector_x, detector_y):
     # use increasing numbers in the data, to make sure blocks have different content
@@ -161,168 +166,167 @@ def save_nxs(filepath: Path, angles, data, detector_x, detector_y):
             angles=b1.angles,
         )
 
-save_nxs("./diamond2_phantom.nxs", angles, projData3D_analyt, Horiz_det, Vert_det)
+save_nxs("./diamond2_phantom.nxs", angles, swapped_projData3D_analyt, Horiz_det, Vert_det)
+
+if recon:
+    input_data_labels = ["detY", "angles", "detX"]
+
+    # transfering numpy array to CuPy array
+    projData3D_analyt_cupy = cp.asarray(projData3D_analyt, order="C")
+    # %%
+    # It is recommend to re-run twice in order to get the optimal time
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print("%%%%%%%%%Reconstructing with 3D FBP-CuPy method %%%%%%%%%%%%")
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    RecToolsCP = RecToolsDIRCuPy(
+        DetectorsDimH=Horiz_det,  # Horizontal detector dimension
+        DetectorsDimH_pad=0,  # Padding size of horizontal detector
+        DetectorsDimV=Vert_det,  # Vertical detector dimension (3D case)
+        CenterRotOffset=0.0,  # Center of Rotation scalar or a vector
+        AnglesVec=angles_rad,  # A vector of projection angles in radians
+        ObjSize=N_size,  # Reconstructed object dimensions (scalar)
+        device_projector="gpu",
+    )
+
+    tic = timeit.default_timer()
+    FBPrec_cupy = RecToolsCP.FBP(
+        projData3D_analyt_cupy,
+        recon_mask_radius=0.95,
+        data_axes_labels_order=input_data_labels,
+        cutoff_freq=0.3,
+    )
+    toc = timeit.default_timer()
+    Run_time = toc - tic
+    print(
+        "FBP 3D reconstruction with FFT filtering using CuPy (GPU) in {} seconds".format(
+            Run_time
+        )
+    )
+
+    # bring data from the device to the host
+    FBPrec_cupy = cp.asnumpy(FBPrec_cupy)
+
+    sliceSel = int(0.5 * N_size)
+    max_val = 1
+    plt.figure()
+    plt.subplot(131)
+    plt.imshow(FBPrec_cupy[sliceSel, :, :], vmin=0, vmax=max_val)
+    plt.title("3D FBP Reconstruction, axial view")
+
+    plt.subplot(132)
+    plt.imshow(FBPrec_cupy[:, sliceSel, :], vmin=0, vmax=max_val)
+    plt.title("3D FBP Reconstruction, coronal view")
+
+    plt.subplot(133)
+    plt.imshow(FBPrec_cupy[:, :, sliceSel], vmin=0, vmax=max_val)
+    plt.title("3D FBP Reconstruction, sagittal view")
+    plt.show()
 
 
-# input_data_labels = ["detY", "angles", "detX"]
+    sliceSel = int(0.5 * N_size)
+    max_val = 0.3
+    plt.figure()
+    plt.subplot(131)
+    plt.imshow(
+        abs(FBPrec_cupy[sliceSel, :, :] - phantom_tm[sliceSel, :, :]), vmin=0, vmax=max_val
+    )
+    plt.title("3D FBP residual, axial view")
 
-# # transfering numpy array to CuPy array
-# projData3D_analyt_cupy = cp.asarray(projData3D_analyt, order="C")
-# # %%
-# # It is recommend to re-run twice in order to get the optimal time
-# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-# print("%%%%%%%%%Reconstructing with 3D FBP-CuPy method %%%%%%%%%%%%")
-# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-# RecToolsCP = RecToolsDIRCuPy(
-#     DetectorsDimH=Horiz_det,  # Horizontal detector dimension
-#     DetectorsDimH_pad=0,  # Padding size of horizontal detector
-#     DetectorsDimV=Vert_det,  # Vertical detector dimension (3D case)
-#     CenterRotOffset=0.0,  # Center of Rotation scalar or a vector
-#     AnglesVec=angles_rad,  # A vector of projection angles in radians
-#     ObjSize=N_size,  # Reconstructed object dimensions (scalar)
-#     device_projector="gpu",
-# )
+    plt.subplot(132)
+    plt.imshow(
+        abs(FBPrec_cupy[:, sliceSel, :] - phantom_tm[:, sliceSel, :]), vmin=0, vmax=max_val
+    )
+    plt.title("3D FBP residual, coronal view")
 
-# tic = timeit.default_timer()
-# FBPrec_cupy = RecToolsCP.FBP(
-#     projData3D_analyt_cupy,
-#     recon_mask_radius=0.95,
-#     data_axes_labels_order=input_data_labels,
-#     cutoff_freq=0.3,
-# )
-# toc = timeit.default_timer()
-# Run_time = toc - tic
-# print(
-#     "FBP 3D reconstruction with FFT filtering using CuPy (GPU) in {} seconds".format(
-#         Run_time
-#     )
-# )
-
-# # bring data from the device to the host
-# FBPrec_cupy = cp.asnumpy(FBPrec_cupy)
-
-# sliceSel = int(0.5 * N_size)
-# max_val = 1
-# plt.figure()
-# plt.subplot(131)
-# plt.imshow(FBPrec_cupy[sliceSel, :, :], vmin=0, vmax=max_val)
-# plt.title("3D FBP Reconstruction, axial view")
-
-# plt.subplot(132)
-# plt.imshow(FBPrec_cupy[:, sliceSel, :], vmin=0, vmax=max_val)
-# plt.title("3D FBP Reconstruction, coronal view")
-
-# plt.subplot(133)
-# plt.imshow(FBPrec_cupy[:, :, sliceSel], vmin=0, vmax=max_val)
-# plt.title("3D FBP Reconstruction, sagittal view")
-# plt.show()
+    plt.subplot(133)
+    plt.imshow(
+        abs(FBPrec_cupy[:, :, sliceSel] - phantom_tm[:, :, sliceSel]), vmin=0, vmax=max_val
+    )
+    plt.title("3D FBP residual, sagittal view")
+    plt.show()
 
 
-# #
-# sliceSel = int(0.5 * N_size)
-# max_val = 0.3
-# plt.figure()
-# plt.subplot(131)
-# plt.imshow(
-#     abs(FBPrec_cupy[sliceSel, :, :] - phantom_tm[sliceSel, :, :]), vmin=0, vmax=max_val
-# )
-# plt.title("3D FBP residual, axial view")
+    print(
+        "Min {} and Max {} of the volume".format(np.min(FBPrec_cupy), np.max(FBPrec_cupy))
+    )
 
-# plt.subplot(132)
-# plt.imshow(
-#     abs(FBPrec_cupy[:, sliceSel, :] - phantom_tm[:, sliceSel, :]), vmin=0, vmax=max_val
-# )
-# plt.title("3D FBP residual, coronal view")
+    # calculate errors
+    Qtools = QualityTools(phantom_tm, FBPrec_cupy)
+    RMSE = Qtools.rmse()
+    print("Root Mean Square Error is {} for FBP".format(RMSE))
 
-# plt.subplot(133)
-# plt.imshow(
-#     abs(FBPrec_cupy[:, :, sliceSel] - phantom_tm[:, :, sliceSel]), vmin=0, vmax=max_val
-# )
-# plt.title("3D FBP residual, sagittal view")
-# plt.show()
+    # %%
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    print("%%%%%%%%%Reconstructing with 3D Fourier-CuPy method %%%%%%%%")
+    print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+    RecToolsCP = RecToolsDIRCuPy(
+        DetectorsDimH=Horiz_det,  # Horizontal detector dimension
+        DetectorsDimH_pad=0,  # Padding size of horizontal detector
+        DetectorsDimV=Vert_det,  # Vertical detector dimension (3D case)
+        CenterRotOffset=0.0,  # Center of Rotation scalar or a vector
+        AnglesVec=angles_rad,  # A vector of projection angles in radians
+        ObjSize=N_size,  # Reconstructed object dimensions (scalar)
+        device_projector="gpu",
+    )
 
+    tic = timeit.default_timer()
+    Fourier_cupy = RecToolsCP.FOURIER_INV(
+        projData3D_analyt_cupy,
+        recon_mask_radius=0.95,
+        data_axes_labels_order=input_data_labels,
+    )
+    toc = timeit.default_timer()
+    Run_time = toc - tic
+    print("Fourier 3D reconstruction using CuPy (GPU) in {} seconds".format(Run_time))
 
-# print(
-#     "Min {} and Max {} of the volume".format(np.min(FBPrec_cupy), np.max(FBPrec_cupy))
-# )
+    # bring data from the device to the host
+    Fourier_cupy = cp.asnumpy(Fourier_cupy)
 
-# # calculate errors
-# Qtools = QualityTools(phantom_tm, FBPrec_cupy)
-# RMSE = Qtools.rmse()
-# print("Root Mean Square Error is {} for FBP".format(RMSE))
+    sliceSel = int(0.5 * N_size)
+    max_val = 1
+    plt.figure()
+    plt.subplot(131)
+    plt.imshow(Fourier_cupy[sliceSel, :, :], vmin=0, vmax=max_val)
+    plt.title("3D Fourier Reconstruction, axial view")
 
-# # %%
-# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-# print("%%%%%%%%%Reconstructing with 3D Fourier-CuPy method %%%%%%%%")
-# print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
-# RecToolsCP = RecToolsDIRCuPy(
-#     DetectorsDimH=Horiz_det,  # Horizontal detector dimension
-#     DetectorsDimH_pad=0,  # Padding size of horizontal detector
-#     DetectorsDimV=Vert_det,  # Vertical detector dimension (3D case)
-#     CenterRotOffset=0.0,  # Center of Rotation scalar or a vector
-#     AnglesVec=angles_rad,  # A vector of projection angles in radians
-#     ObjSize=N_size,  # Reconstructed object dimensions (scalar)
-#     device_projector="gpu",
-# )
+    plt.subplot(132)
+    plt.imshow(Fourier_cupy[:, sliceSel, :], vmin=0, vmax=max_val)
+    plt.title("3D Fourier Reconstruction, coronal view")
 
-# tic = timeit.default_timer()
-# Fourier_cupy = RecToolsCP.FOURIER_INV(
-#     projData3D_analyt_cupy,
-#     recon_mask_radius=0.95,
-#     data_axes_labels_order=input_data_labels,
-# )
-# toc = timeit.default_timer()
-# Run_time = toc - tic
-# print("Fourier 3D reconstruction using CuPy (GPU) in {} seconds".format(Run_time))
-
-# # bring data from the device to the host
-# Fourier_cupy = cp.asnumpy(Fourier_cupy)
-
-# sliceSel = int(0.5 * N_size)
-# max_val = 1
-# plt.figure()
-# plt.subplot(131)
-# plt.imshow(Fourier_cupy[sliceSel, :, :], vmin=0, vmax=max_val)
-# plt.title("3D Fourier Reconstruction, axial view")
-
-# plt.subplot(132)
-# plt.imshow(Fourier_cupy[:, sliceSel, :], vmin=0, vmax=max_val)
-# plt.title("3D Fourier Reconstruction, coronal view")
-
-# plt.subplot(133)
-# plt.imshow(Fourier_cupy[:, :, sliceSel], vmin=0, vmax=max_val)
-# plt.title("3D Fourier Reconstruction, sagittal view")
-# plt.show()
+    plt.subplot(133)
+    plt.imshow(Fourier_cupy[:, :, sliceSel], vmin=0, vmax=max_val)
+    plt.title("3D Fourier Reconstruction, sagittal view")
+    plt.show()
 
 
-# sliceSel = int(0.5 * N_size)
-# max_val = 0.3
-# plt.figure()
-# plt.subplot(131)
-# plt.imshow(
-#     abs(Fourier_cupy[sliceSel, :, :] - phantom_tm[sliceSel, :, :]), vmin=0, vmax=max_val
-# )
-# plt.title("3D Fourier residual, axial view")
+    sliceSel = int(0.5 * N_size)
+    max_val = 0.3
+    plt.figure()
+    plt.subplot(131)
+    plt.imshow(
+        abs(Fourier_cupy[sliceSel, :, :] - phantom_tm[sliceSel, :, :]), vmin=0, vmax=max_val
+    )
+    plt.title("3D Fourier residual, axial view")
 
-# plt.subplot(132)
-# plt.imshow(
-#     abs(Fourier_cupy[:, sliceSel, :] - phantom_tm[:, sliceSel, :]), vmin=0, vmax=max_val
-# )
-# plt.title("3D Fourier residual, coronal view")
+    plt.subplot(132)
+    plt.imshow(
+        abs(Fourier_cupy[:, sliceSel, :] - phantom_tm[:, sliceSel, :]), vmin=0, vmax=max_val
+    )
+    plt.title("3D Fourier residual, coronal view")
 
-# plt.subplot(133)
-# plt.imshow(
-#     abs(Fourier_cupy[:, :, sliceSel] - phantom_tm[:, :, sliceSel]), vmin=0, vmax=max_val
-# )
-# plt.title("3D Fourier residual, sagittal view")
-# plt.show()
+    plt.subplot(133)
+    plt.imshow(
+        abs(Fourier_cupy[:, :, sliceSel] - phantom_tm[:, :, sliceSel]), vmin=0, vmax=max_val
+    )
+    plt.title("3D Fourier residual, sagittal view")
+    plt.show()
 
 
-# print(
-#     "Min {} and Max {} of the volume".format(np.min(FBPrec_cupy), np.max(FBPrec_cupy))
-# )
+    print(
+        "Min {} and Max {} of the volume".format(np.min(FBPrec_cupy), np.max(FBPrec_cupy))
+    )
 
-# # calculate errors
-# Qtools = QualityTools(phantom_tm, Fourier_cupy)
-# RMSE = Qtools.rmse()
-# print("Root Mean Square Error is {} for Fourier inversion".format(RMSE))
+    # calculate errors
+    Qtools = QualityTools(phantom_tm, Fourier_cupy)
+    RMSE = Qtools.rmse()
+    print("Root Mean Square Error is {} for Fourier inversion".format(RMSE))
